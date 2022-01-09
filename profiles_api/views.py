6c7e4a85ce47708369b69cbf86b29f123bd7e670
Owa <ramosmarkjoshua@gmail.com>
2022-01-09 17:15:49 +0800

    Added POST, PUT, PATCH, and DELETE method to our APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,15 @@
 from rest_framework.views import APIView #Rest framework is the Djano rest framework that we restored previously in requirements.txt
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
+    #Adding get function
     def get(self, request, format=None):
         """Returns a list of APIView features"""
         an_apiview = [
@@ -15,3 +20,31 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message':'Hello!','an_apiview':an_apiview})
+
+    #Adding Post function
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        #Validating serializer
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None): #provides all of the update in the request including not given ones (blanks)
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None): #provides what is only provided in the request
+        """Handle a partial update of an object"""
+        return Response({'method':'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method':'DELETE'})
